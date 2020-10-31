13c1bca007db4322443e27a9cc1cca6c942b4441
Abdullah272056 <abdullah272056@gmail.com>
2020-10-31 17:49:02 +0600

    create CustomAdapter2 for RecyclerView

---
+++ app/src/main/java/com/example/firebasedemo/CustomAdapter2.java
@@ -0,0 +1,51 @@
+package com.example.firebasedemo;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CustomAdapter2 extends RecyclerView.Adapter<CustomAdapter2.MyViewHolder> {
+    Context context;
+    private List<Student> studentList;
+
+    public CustomAdapter2(Context context, List<Student> studentList) {
+        this.context = context;
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater= LayoutInflater.from(context);
+        View view= layoutInflater.inflate(R.layout.sample_layout,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.nameTextView.setText(studentList.get(position).getName());
+        holder.departmentTextView.setText(studentList.get(position).getDepartment());
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView,departmentTextView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView=itemView.findViewById(R.id.nameTextViewId);
+            departmentTextView=itemView.findViewById(R.id.departmentTextViewId);
+        }
+    }
+}
