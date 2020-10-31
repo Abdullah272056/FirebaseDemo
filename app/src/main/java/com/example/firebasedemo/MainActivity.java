d11ab0aa5dc3d8e2a099cfe9a0ce0894266fc327
Abdullah272056 <abdullah272056@gmail.com>
2020-10-31 20:09:42 +0600

    retrieve data and show main page

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.firebasedemo;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelStore;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -27,16 +27,24 @@ public class MainActivity extends AppCompatActivity {
     EditText nameEditText;
     Button saveButton,detailsBtn,detailsBtn2;
     Spinner departmentSpinner;
-    DatabaseReference databaseStudent;
+    DatabaseReference databaseReference;
+    ListView listView;
+    List<Student> studentList;
+    CustomAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        databaseStudent= FirebaseDatabase.getInstance().getReference("student");
+        databaseReference= FirebaseDatabase.getInstance().getReference("student");
         nameEditText=findViewById(R.id.nameEditTextId);
         saveButton=findViewById(R.id.saveButtonId);
         detailsBtn=findViewById(R.id.detailsBtnId);
         detailsBtn2=findViewById(R.id.detailsBtnId2);
+
+        studentList=new ArrayList<>();
+        customAdapter=new CustomAdapter(MainActivity.this,studentList);
+        listView=findViewById(R.id.mainPageListViewId);
+
         departmentSpinner=findViewById(R.id.departmentSpinnerId);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,13 +69,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                studentList.clear();
+                for (DataSnapshot studentSnapshot:snapshot.getChildren()){
+                    Student student=studentSnapshot.getValue(Student.class);
+                    studentList.add(student);
+                }
+                listView.setAdapter(customAdapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     private void addStudentInfo() {
         String name=nameEditText.getText().toString();
         String department= departmentSpinner.getSelectedItem().toString();
         if (!TextUtils.isEmpty(name)){
-        String id=databaseStudent.push().getKey();
+        String id=databaseReference.push().getKey();
         Student student=new Student(id,name,department);
-        databaseStudent.child(id).setValue(student);
+            databaseReference.child(id).setValue(student);
             Toast.makeText(this, "insert success", Toast.LENGTH_SHORT).show();
         }else {
             Toast.makeText(this, "name is empty", Toast.LENGTH_SHORT).show();
