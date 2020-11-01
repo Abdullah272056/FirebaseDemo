bf6ac4cc2a82776767e52a8b12c9c922c05117b5
Abdullah272056 <abdullah272056@gmail.com>
2020-11-01 15:40:14 +0600

    New data add function against student name with same id

--- app/src/main/java/com/example/firebasedemo/AddSemesterActivity.java
+++ app/src/main/java/com/example/firebasedemo/AddSemesterActivity.java
@@ -3,10 +3,15 @@ package com.example.firebasedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddSemesterActivity extends AppCompatActivity {
     TextView studentNameTextView;
@@ -14,6 +19,7 @@ public class AddSemesterActivity extends AppCompatActivity {
     Spinner semesterSpinner;
     EditText cgpaEditText;
     String name, id;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,30 @@ public class AddSemesterActivity extends AppCompatActivity {
         name=getIntent().getStringExtra("name");
         studentNameTextView.setText(name);
 
+        databaseReference= FirebaseDatabase.getInstance().getReference("Semester").child(id);
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSemester();
+            }
+        });
+
+
+
+    }
+
+    private void saveSemester() {
+        String semester=semesterSpinner.getSelectedItem().toString();
+        String cgpa=cgpaEditText.getText().toString();
+        if (!TextUtils.isEmpty(cgpa)){
+            String id=databaseReference.push().getKey();
+            StudentSemester studentSemester=new StudentSemester(id,semester,cgpa);
+            databaseReference.child(id).setValue(studentSemester);
+        }
+        else {
+            cgpaEditText.setError("Enter a value");
+        }
 
     }
 }
