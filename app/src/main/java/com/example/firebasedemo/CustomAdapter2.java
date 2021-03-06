5737625876886902ac71aec431bb1583c593e09c
Abdullah32838 <abdullahalaman70@gmail.com>
2021-03-06 13:24:07 +0600

    add recycler_view_item_operation.xml

--- app/src/main/java/com/example/firebasedemo/CustomAdapter2.java
+++ app/src/main/java/com/example/firebasedemo/CustomAdapter2.java
@@ -6,16 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class CustomAdapter2 extends RecyclerView.Adapter<CustomAdapter2.MyViewHolder> {
     Context context;
     private List<Student> studentList;
-
+    private static DatabaseReference mDatabase;
     public CustomAdapter2(Context context, List<Student> studentList) {
         this.context = context;
         this.studentList = studentList;
@@ -30,9 +34,25 @@ public class CustomAdapter2 extends RecyclerView.Adapter<CustomAdapter2.MyViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         holder.nameTextView.setText("Name: "+studentList.get(position).getName());
         holder.departmentTextView.setText("Dept: "+studentList.get(position).getDepartment());
+    holder.itemView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
+            mDatabase= FirebaseDatabase.getInstance().getReference("student");
+
+               //mDatabase.child(studentList.get(position).getId()).setValue(null);
+
+                // update data by id
+               mDatabase.child(studentList.get(position).getId()).setValue(new Student(studentList.get(position).getId(),studentList.get(position).getName(),"Bolbona"));
+
+              // mDatabase.child(studentList.get(position).getId()).removeValue();
+
+
+        }
+    });
     }
 
     @Override
